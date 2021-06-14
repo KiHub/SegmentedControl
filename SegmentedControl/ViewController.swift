4b46345385eec55a87d2b6a3c04d26d63673b5be
Kirill Ki <kirillkalapov@gmail.com>
2021-06-14 17:35:01 +0200

    Done!

--- SegmentedControl/ViewController.swift
+++ SegmentedControl/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
+        
+        
     }
 
 
+    @IBAction func choiseSegment(_ sender: UISegmentedControl) {
+        label.isHidden = false
+        switch segmentedControl.selectedSegmentIndex {
+        case 0 :
+            label.text = "First segment"
+        case 1 :
+            label.text = "Second segment"
+     
+        default:
+            label.text = "Third segment"
+        }
+        
+    }
 }
 
